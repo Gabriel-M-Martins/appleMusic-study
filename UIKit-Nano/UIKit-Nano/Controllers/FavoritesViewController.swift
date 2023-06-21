dd90178eb84113965e7a6e4aab084b65b2408731
Eduardo Brum <eduardofilot@hotmail.com>
2023-06-21 16:56:54 -0300

    favorites storyboard file

---
+++ UIKit-Nano/UIKit-Nano/Controllers/FavoritesViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FavoritesViewController.swift
+//  UIKit-Nano
+//
+//  Created by Eduardo Filot Brum on 21/06/23.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var favoritesTableView: UITableView!
+    
+    @IBOutlet weak var buttonTeste: UIButton!
+    let searchController = UISearchController()
+    
+    @ObservableObject var favoriteMusics = MusicService.shared.favoriteMusics
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchController.searchBar.placeholder = "Artists, Songs, Lyrics, and More"
+        navigationItem.searchController = searchController
+        
+        favoritesTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard !favoriteMusics.isEmpty else { return 1 }
+        
+        return favoriteMusics.count
+    }
+    
+    @IBAction func favorite(_ sender: Any) {
+//        print(MusicService.shared.getAllMusics().first)
+        guard let music = MusicService.shared.getAllMusics().first else { return }
+        MusicService.shared.toggleFavorite(music: music, isFavorite: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        
+        if favoriteMusics.isEmpty {
+            cell = favoritesTableView.dequeueReusableCell(withIdentifier: "EmptyFavoritesCell", for: indexPath)
+        } else {
+            let music = favoriteMusics[indexPath.row]
+            let favoriteButton = UIImageView(image: UIImage(systemName: "heart.fill"))
+            favoriteButton.tintColor = .systemPink
+            
+            cell = favoritesTableView.dequeueReusableCell(withIdentifier: "FavoriteMusicCell", for: indexPath)
+            
+            cell.imageView?.image = MusicService.shared.getCoverImage(forItemIded: music.id)
+            cell.imageView?.layer.cornerRadius = 4
+            
+            cell.textLabel?.text = music.title
+            
+            cell.detailTextLabel?.text = music.artist
+            
+            cell.accessoryView = favoriteButton
+            
+        }
+        
+        return cell
+    }
+}
